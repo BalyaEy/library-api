8408632253c7ea450bf4357759e01fd8a34a4432
Eyüp Boyacı <eyup.boyaci@hotmail.com>
2025-07-01 23:13:25 +0300

    Add Role enum, User entity, UserRepository and database configuration

---
+++ src/main/java/com/yetgim/library_api/entity/User.java
@@ -0,0 +1,30 @@
+package com.yetgim.library_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
+
