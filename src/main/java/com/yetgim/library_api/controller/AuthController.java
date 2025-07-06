b2f7e3170cde493778fb962d9d8806c107df27e1
Eyüp Boyacı <eyup.boyaci@hotmail.com>
2025-07-06 23:43:57 +0300

    Implement user registration with security config, DTOs, and controller setup

--- src/main/java/com/yetgim/library_api/controller/AuthController.java
+++ src/main/java/com/yetgim/library_api/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.yetgim.library_api.dto.RegisterRequest;
 import com.yetgim.library_api.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
     private final AuthenticationService authenticationService;
 
-        public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
-        AuthResponse response = authenticationService.register(request);
-        return ResponseEntity.ok(response);
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
     }
 }
+
