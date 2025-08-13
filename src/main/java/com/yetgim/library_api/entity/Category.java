8e03ecf97e5f948c56f40da074a8b95fe829002d
Eyüp Boyacı <eyup.boyaci@hotmail.com>
2025-08-13 18:09:58 +0300

    feat: Add Author and Category entities and update Book entity

---
+++ src/main/java/com/yetgim/library_api/entity/Category.java
@@ -0,0 +1,21 @@
+package com.yetgim.library_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
